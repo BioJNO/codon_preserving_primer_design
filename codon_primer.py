0cd1dca3d73b86d0cad79205c7a512e44c880eb7
BioJNO <JamieNeilOrr@gmail.com>
2018-10-09 16:07:42 +0100

    PEP8 style updates and bug fixes

--- codon_primer.py
+++ codon_primer.py
@@ -1,84 +1,132 @@
 #!/usr/bin/env python
 
 from Bio.Seq import Seq
-from Bio.Alphabet import generic_dna 
+from Bio.Alphabet import generic_dna
 from Bio.SeqIO.FastaIO import SimpleFastaParser
 import primer3
 import argparse
+from collections import defaultdict
 
-parser = argparse.ArgumentParser(description = "given a fasta file of coding sequences presents all possible PCR primers which preserve the coding sense of the sequence")
-parser.add_argument('fasta', help = "path to fasta file containing desired coding sequences")
-parser.add_argument('-pl', type=int, default = 22, help = "desired length of primers, default=22")
-parser.add_argument('-f', type=int, default=0, help = "number of base pairs flanking coding region, default = 0")
-parser.add_argument('-o', type=str, default="CDS_primers_out.txt", help = "specify output filename, default = CDS_primers_out.txt")
-parser.add_argument('-tmax', type=int, default=58, help = "maximum melting temp for primers to report, default = 58")
-parser.add_argument('-tmin', type=int, default=52, help = "minimum melting temp for primers to report, default = 52")
-parser.add_argument('-os', default = "single_rec_output.txt", help = "output table for unique primers, default = single_rec_output.txt")
-parser.add_argument('-om', default = "multi_rec_output.txt", help = "output table for non-unique primers, default = multi_rec_output.txt")
+parser = argparse.ArgumentParser(description="Given a fasta file of coding "
+                                             "sequences presents all possible"
+                                             " PCR primers which preserve the"
+                                             " coding sense of the sequence")
+parser.add_argument('fasta',
+                    help="path to fasta file containing "
+                         "desired coding sequences")
+parser.add_argument('-pl', type=int,
+                    default=22,
+                    help="desired length of primers, "
+                         "default=22")
+parser.add_argument('-f', type=int,
+                    default=0,
+                    help="number of base pairs flanking"
+                         " coding region, "
+                         "default = 0")
+parser.add_argument('-o', type=str,
+                    default="CDS_primers_out.txt",
+                    help="specify output filename, "
+                         "default = CDS_primers_out.txt")
+parser.add_argument('-tmax', type=int, default=58,
+                    help="maximum melting temp for primers to report, "
+                    "default = 58")
+parser.add_argument('-tmin', type=int, default=52,
+                    help="minimum melting temp for primers to report, "
+                    "default = 52")
+parser.add_argument('-os',
+                    default="single_rec_output.txt",
+                    help="output table for unique primers, "
+                    "default = single_rec_output.txt")
+parser.add_argument('-om',
+                    default="multi_rec_output.txt",
+                    help="output table for non-unique primers, "
+                         "default = multi_rec_output.txt")
 
 args = parser.parse_args()
 
 seqhandle = open(args.fasta)
 outfile = open(args.o, "w")
-outfile.write("Target CDS\tPrimer type\tprimer sequence\tTm\tprimer length\tamp start pos\tframelen\torientation\n")
+outfile.write("Target CDS\t"
+              "Primer type\t"
+              "primer sequence\t"
+              "Tm\t"
+              "primer length\t"
+              "amp start pos\t"
+              "framelen\t"
+              "orientation\n")
 
 poslist = ['ATG', 'GTG', 'TTG']
 neglist = ['TTA', 'CTA', 'TCA']
 
 for title, seq in SimpleFastaParser(seqhandle):
-	lenseq = len(seq)
-	frame = seq[args.f:lenseq -  args.f].upper()
-	if frame[:3] in poslist:
-		orientation = 1
-	elif frame[:3] in neglist:
-		orientation = -1
-	framelen = len(frame)
-	postframe = seq[lenseq - args.f:].upper()
-	preframe = seq[: args.f].upper()
-	preframe_obj = Seq(preframe, generic_dna)
-	preframerev = str(preframe_obj.reverse_complement())
-	frame_obj = Seq(frame, generic_dna)
-	framerev = str(frame_obj.reverse_complement())
-	postframe_obj = Seq(postframe, generic_dna)
-	postframerev = str(postframe_obj.reverse_complement())
-	
-	for index in range(0, args.f - args.pl, 1):
-		fcandidate = preframe[index:index+args.pl]
-		fcand_len = str(len(fcandidate))
-		Tm = int(primer3.calcTm(fcandidate))
-		if Tm > args.tmin and Tm < args.tmax:
-			Hairpin = primer3.calcHairpin(fcandidate)
-			if Hairpin.structure_found is False:
-				outfile.write(title + "\tPreframe forward\t" + fcandidate + "\t" + str(Tm) + "\t" + fcand_len + "\t" + str(index) + "\t" + str(framelen) + "\t" + str(orientation) + "\n")
-		
-	for index in range(0, framelen - args.pl, 3):
-		fcandidate = frame[index:index+args.pl]
-		fcand_len = str(len(fcandidate))
-		Tm = int(primer3.calcTm(fcandidate))
-		if Tm > args.tmin and Tm < args.tmax:
-			Hairpin = primer3.calcHairpin(fcandidate)
-			if Hairpin.structure_found is False:
-				outfile.write(title + "\tIn frame forward\t" + fcandidate + "\t " + str(Tm) + "\t" + fcand_len + "\t" + str(index + args.f) + "\t" + str(framelen) + "\t" + str(orientation) + "\n")
-			
-	for index in range(0, framelen - args.pl, 3):
-		rcandidate = framerev[index:index+args.pl]
-		rcand_len = str(len(rcandidate))
-		Tm = int(primer3.calcTm(rcandidate))
-		if Tm > args.tmin and Tm < args.tmax:
-			Hairpin = primer3.calcHairpin(rcandidate)
-			if Hairpin.structure_found is False:
-				outfile.write(title + "\tIn frame reverse\t" + rcandidate + "\t" + str(Tm) + "\t" + rcand_len + "\t" + str(args.f + (framelen - index)) + "\t" + str(framelen) + "\t" + str(orientation) + "\n")
-			
-	for index in range(0, args.f - args.pl, 1):
-		rcandidate = postframerev[index:index+args.pl]
-		rcand_len = str(len(rcandidate))
-		Tm = int(primer3.calcTm(rcandidate))
-		if Tm > args.tmin and Tm < args.tmax:
-			Hairpin = primer3.calcHairpin(rcandidate)
-			if Hairpin.structure_found is False:
-				outfile.write(title + "\tPost frame reverse\t" + rcandidate + "\t" + str(Tm) + "\t" + rcand_len + "\t" + str(args.f + framelen + (args.f - index)) + "\t" + str(framelen) + "\t" + str(orientation) + "\n")
+    lenseq = len(seq)
+    frame = seq[args.f:lenseq - args.f].upper()
+    if frame[:3] in poslist:
+        orientation = 1
+    elif frame[:3] in neglist:
+        orientation = -1
+    framelen = len(frame)
+    postframe = seq[lenseq - args.f:].upper()
+    preframe = seq[: args.f].upper()
+    preframe_obj = Seq(preframe, generic_dna)
+    preframerev = str(preframe_obj.reverse_complement())
+    frame_obj = Seq(frame, generic_dna)
+    framerev = str(frame_obj.reverse_complement())
+    postframe_obj = Seq(postframe, generic_dna)
+    postframerev = str(postframe_obj.reverse_complement())
 
+    for index in range(0, args.f - args.pl, 1):
+        fcandidate = preframe[index:index+args.pl]
+        fcand_len = str(len(fcandidate))
+        Tm = int(primer3.calcTm(fcandidate))
+        if Tm > args.tmin and Tm < args.tmax:
+            Hairpin = primer3.calcHairpin(fcandidate)
+            if Hairpin.structure_found is False:
+                outfile.write(title +
+                              "\tPreframe forward\t" +
+                              fcandidate + "\t" + str(Tm) + "\t" +
+                              fcand_len + "\t" + str(index) + "\t" +
+                              str(framelen) + "\t" + str(orientation) + "\n")
 
+    for index in range(0, framelen - args.pl, 3):
+        fcandidate = frame[index:index+args.pl]
+        fcand_len = str(len(fcandidate))
+        Tm = int(primer3.calcTm(fcandidate))
+        if Tm > args.tmin and Tm < args.tmax:
+            Hairpin = primer3.calcHairpin(fcandidate)
+            if Hairpin.structure_found is False:
+                outfile.write(title + "\tIn frame forward\t" +
+                              fcandidate + "\t " + str(Tm) + "\t" +
+                              fcand_len + "\t" + str(index + args.f) + "\t" +
+                              str(framelen) + "\t" + str(orientation) + "\n")
+
+    for index in range(0, framelen - args.pl, 3):
+        rcandidate = framerev[index:index+args.pl]
+        rcand_len = str(len(rcandidate))
+        Tm = int(primer3.calcTm(rcandidate))
+        if Tm > args.tmin and Tm < args.tmax:
+            Hairpin = primer3.calcHairpin(rcandidate)
+            if Hairpin.structure_found is False:
+                outfile.write(title + "\tIn frame reverse\t" +
+                              rcandidate + "\t" + str(Tm) + "\t" +
+                              rcand_len + "\t" +
+                              str(args.f + (framelen - index)) +
+                              "\t" + str(framelen) + "\t" +
+                              str(orientation) + "\n")
+
+    for index in range(0, args.f - args.pl, 1):
+        rcandidate = postframerev[index:index+args.pl]
+        rcand_len = str(len(rcandidate))
+        Tm = int(primer3.calcTm(rcandidate))
+        if Tm > args.tmin and Tm < args.tmax:
+            Hairpin = primer3.calcHairpin(rcandidate)
+            if Hairpin.structure_found is False:
+                outfile.write(title + "\tPost frame reverse\t" +
+                              rcandidate + "\t" + str(Tm) + "\t" +
+                              rcand_len + "\t" +
+                              str(args.f + framelen + (args.f - index)) +
+                              "\t" + str(framelen) + "\t" +
+                              str(orientation) + "\n")
 
 outfile.close()
 
@@ -90,32 +138,26 @@ multioutfile = open(args.om, "w")
 primer_dict = defaultdict(int)
 
 for line in primertab:
-	columns = line.split("\t")
-	primer = columns[2]
-	primer_dict[primer] += 1
+    columns = line.split("\t")
+    primer = columns[2]
+    primer_dict[primer] += 1
 
 multiples = {}
 
 for key, vals in primer_dict.items():
-	if vals > 1:
-		print(key,vals)
-		multiples[key] = vals
-
+    if vals > 1:
+        print(key, vals)
+        multiples[key] = vals
 
 primertab = open(args.input, "r")
 
 for line in primertab:
-	columns = line.split("\t")
-	primer = columns[2]
-	if primer in multiples.keys():
-		multioutfile.write(line)
-	else:
-		singleoutfile.write(line)
-
+    columns = line.split("\t")
+    primer = columns[2]
+    if primer in multiples.keys():
+        multioutfile.write(line)
+    else:
+        singleoutfile.write(line)
 
 multioutfile.close()
 singleoutfile.close()
-
-
-
-
--- grabflanks.py
+++ grabflanks.py
@@ -1,109 +1,123 @@
 #!/usr/bin/env python
-
+from Bio.Seq import Seq
+from Bio.Alphabet import generic_dna
+from Bio.SeqIO.FastaIO import SimpleFastaParser
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description = "pull out desired genes from genomic sequence with flanking regions")
-parser.add_argument('genes', help = 'path to fasta file containing desired gene sequences')
-parser.add_argument('contigs', help = 'path to fasta file containing genomic contigs')
-parser.add_argument('-f', type=int, default = 200, help = 'desired length of genomic flanking region to return either side of desired gene sequences, default=200')
-parser.add_argument('-o', default="grabflanksout", help = 'path to desired output directory, default=grabflanksout')
-parser.add_argument('-v', action="count", default=0, help = 'set verbose mode, default=off')
-parser.add_argument('--prefix', default = "genes", help = 'add output file prefix, default=genes')
-parser.add_argument('-s', action="count", default=0, help = "include to write each recovered gene with falnking region as a single fasta file, default=off")
-parser.add_argument('-c', action="count", default=0, help = "include to write config files to pass to find_differential_primers.py for each gene flanking region, requires -s, default=off")
+parser = argparse.ArgumentParser(
+    description="pull out desired genes from genomic "
+                "sequence with flanking regions")
+parser.add_argument("genes",
+                    help="path to fasta file containing "
+                         "desired gene sequences")
+parser.add_argument("contigs",
+                    help="path to fasta file containing genomic contigs")
+parser.add_argument("-f", type=int, default=200,
+                    help="desired length of genomic flanking region to return"
+                    "either side of desired gene sequences, "
+                    "default=200")
+parser.add_argument("-o", default="grabflanksout",
+                    help="path to desired output directory,"
+                         "default=grabflanksout")
+parser.add_argument("-v", action="count", default=0,
+                    help="set verbose mode, default=off")
+parser.add_argument("--prefix", default="genes",
+                    help="add output file prefix, "
+                    "default=genes")
+parser.add_argument("-s", action="count", default=0,
+                    help="include to write each recovered gene with flanking "
+                         "region as a single fasta file, "
+                         "default=off")
+
 args = parser.parse_args()
 
 genehandle = open(args.genes)
 genomefilename = args.contigs
 os.mkdir(args.o)
 
-if args.c >=1:
-	shellout = open("diff_primers.sh", "w")
+if args.c >= 1:
+    shellout = open("diff_primers.sh", "w")
 
-if args.s >=1:
-	record_path = os.path.join(args.o, "single_fasta")
-	os.mkdir(record_path)
+if args.s >= 1:
+    record_path = os.path.join(args.o, "single_fasta")
+    os.mkdir(record_path)
 
-
-outfile = open(os.path.join(args.o, args.prefix + "_w_flanking_regions.fasta"), "w")
+outfile = open(os.path.join(args.o, args.prefix +
+                            "_w_flanking_regions.fasta"), "w")
 tabout = open(os.path.join(args.o, "grabflanks_tabular_out.csv"), "w")
-tabout.write("gene name,contigname,gene start,gene end,strand,gene length\n")
+tabout.write("gene name,"
+             "contigname,"
+             "gene start,"
+             "gene end,"
+             "orientation,"
+             "gene length\n")
+
 logfile = open(os.path.join(args.o, "grabflanks_log.txt"), "w")
 
-from Bio.Seq import Seq
-from Bio.Alphabet import generic_dna 
-from Bio.SeqIO.FastaIO import SimpleFastaParser
 
-def grabflanks (genefasta, genomefasta, flank):
-	for title, seq in SimpleFastaParser(genefasta):
-		genname = title
-		genseq = Seq(seq, generic_dna)
-		genstring = str(genseq)
-		genseqrev = str(genseq.reverse_complement())
-		genlen = len(genstring)
-		if args.v >= 1:
-			print("length of " + genname + " is " + str(genlen))
-		logfile.write("length of " + genname + " is " + str(genlen) + "\n")
-		genome = open(genomefasta)
-		
-		for title,seq in SimpleFastaParser(genome):
-			if args.v >= 1:
-				print("searching for " + genname + " in contig " + title)
-			findgen = seq.find(genstring.upper())
-			findgenrev = seq.find(genseqrev.upper())
-			contigname = title.replace("|", "_")
-			contigseq = seq
-			
-			if findgen > -1:
-				end = findgen + genlen
-				if args.v >= 1:
-					print("found " + genname + " in contig " + contigname + " from " + str(findgen) + " to " + str(end) + " on the forward strand")
-				logfile.write("found " + genname + " in contig " + contigname + " from " + str(findgen) + " to " + str(end)  + " on the forward strand\n")
-				genflank = contigseq[findgen - flank:findgen+genlen + flank]
-				flankname = genname + "_" + contigname + "_flanking"
-				outfile.write(">%s\n%s\n" % (flankname, genflank))
-				tabout.write("%s,%s,%s,%s,forward,%s\n" % (genname, contigname, findgen, end, genlen))
-				
-				if args.s >= 1:
-					singlepath = os.path.join(record_path, flankname + ".fasta")
-					singleout = open(singlepath, "w")
-					singleout.write(">%s\n%s\n" % (flankname, genflank))
-					
-					if args.c >=1:
-						configname = os.path.join(args.o, genname + "_config.txt")
-						configout = open(configname, "w")
-						configout.write("%s\t%s\t%s\t-\t-\t-\n" % (genname, genname, singlepath))
-						shellout.write("\nfind_differential_primers.py -i " + configname + " -o " + genname + "_amplicon_primers -v --prodigal ${PRODIGAL} --eprimer3 ${EPRIMER3} --blast_exe ${BLAST} --blastdb nt --cpus 8 --noprodigal --nocds --psizemin " + str(genlen) + " --psizemax " + str(genlen + args.f) + " --target " + str(args.f) + "," + str(genlen + args.f) + "\nwait\n")
-						
-			elif findgenrev > -1:
-				end = findgenrev + genlen
-				if args.v >=1:
-					print("found " + genname + " in contig " + contigname + " from " + str(findgenrev) + " to " + str(end) + " on the reverse strand")
-				logfile.write("found " + genname + " in contig " + contigname + " from " + str(findgenrev) + " to " + str(end) + " on the reverse strand\n")
-				genflank = contigseq[findgenrev - flank:findgenrev+genlen + flank]
-				flankname = genname + "_" + contigname + "_flanking"
-				outfile.write(">%s\n%s\n" % (flankname, genflank))
-				tabout.write("%s,%s,%s,%s,reverse,%s\n" % (genname, contigname, findgenrev, end, genlen))
-				
-				if args.s >= 1:
-					singlepath = os.path.join(record_path, flankname + ".fasta")
-					singleout = open(singlepath, "w")
-					singleout.write(">%s\n%s\n" % (flankname, genflank))
-					
-					if args.c >= 1:
-						configname = os.path.join(args.o, genname + "_config.txt")
-						configout = open(configname, "w")
-						configout.write("%s\t%s\t%s\t-\t-\t-\n" % (genname, genname, singlepath))
-						shellout.write("\nfind_differential_primers.py -i " + configname + " -o " + genname + "_amplicon_primers -v --prodigal ${PRODIGAL} --eprimer3 ${EPRIMER3} --blast_exe ${BLAST} --blastdb nt --cpus 8 --noprodigal --nocds --psizemin " + str(genlen) + " --psizemax " + str(genlen + args.f) + " --target " + str(args.f) + "," + str(genlen + args.f) + "\nwait\n")
+def grabflanks(genefasta, genomefasta, flank):
+    for title, seq in SimpleFastaParser(genefasta):
+        genname = title
+        genseq = Seq(seq, generic_dna)
+        genstring = str(genseq)
+        genseqrev = str(genseq.reverse_complement())
+        genlen = len(genstring)
+        if args.v >= 1:
+            print("length of " + genname + " is " + str(genlen))
+        logfile.write("length of " + genname + " is " + str(genlen) + "\n")
+        genome = open(genomefasta)
 
-	
-	
+        for title, seq in SimpleFastaParser(genome):
+            if args.v >= 1:
+                print("searching for " + genname + " in contig " + title)
+            findgen = seq.find(genstring.upper())
+            findgenrev = seq.find(genseqrev.upper())
+            contigname = title.replace("|", "_")
+            contigseq = seq
+
+            if findgen > -1:
+                end = findgen + genlen
+                if args.v >= 1:
+                    logfile.write("found " + genname + " in contig " +
+                                  contigname + " from " + str(findgen) +
+                                  " to " + str(end) +
+                                  " in forward orientation\n")
+                genflank = contigseq[findgen - flank:findgen+genlen + flank]
+                flankname = genname + "_" + contigname + "_flanking"
+                outfile.write(">%s\n%s\n" % (flankname, genflank))
+                tabout.write("%s,%s,%s,%s,forward,%s\n" %
+                             (genname, contigname, findgen, end, genlen))
+
+                if args.s >= 1:
+                    singlepath = os.path.join(
+                        record_path, flankname + ".fasta")
+                    singleout = open(singlepath, "w")
+                    singleout.write(">%s\n%s\n" % (flankname, genflank))
+
+            elif findgenrev > -1:
+                end = findgenrev + genlen
+                if args.v >= 1:
+                    logfile.write("found " + genname + " in contig " +
+                                  contigname + " from " + str(findgenrev) +
+                                  " to " + str(end) +
+                                  " in reverse orientation\n")
+                genflank = contigseq[findgenrev -
+                                     flank:findgenrev+genlen + flank]
+                flankname = genname + "_" + contigname + "_flanking"
+                outfile.write(">%s\n%s\n" % (flankname, genflank))
+                tabout.write("%s,%s,%s,%s,reverse,%s\n" %
+                             (genname, contigname, findgenrev, end, genlen))
+
+                if args.s >= 1:
+                    singlepath = os.path.join(
+                        record_path, flankname + ".fasta")
+                    singleout = open(singlepath, "w")
+                    singleout.write(">%s\n%s\n" % (flankname, genflank))
+
+
 grabflanks(genehandle, genomefilename, args.f)
-
-
 
 outfile.close()
 logfile.close()
 tabout.close()
-
